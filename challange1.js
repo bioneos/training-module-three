e3ae5d34244e30221ee4e9de3130a3d557047f95
Schaden-Cweitzer <cdschweitz@gmail.com>
2019-06-19 09:27:20 -0500

    Added a find the bug challange with JavaScript.

---
+++ challange1.js
@@ -0,0 +1,20 @@
+var count = 4;
+
+function foo() {
+    var table = [count];
+
+    /* Build the table. */
+    while (count-- > 0) {
+        table.push([]);
+    }
+
+    /* Fill it with numbers. */
+    for (var count = 1; count < table.length; count++) {
+        table[count].push(count);
+    }
+    return table;
+}
+
+foo(); // What does this return? And why?
+//Answer here:
+//https://nullprogram.com/blog/2012/11/19/ 
