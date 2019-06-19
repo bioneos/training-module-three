f8d8916ebbf17538bd0c20d239268af199504959
Schaden-Cweitzer <cdschweitz@gmail.com>
2019-06-19 10:27:48 -0500

    Added a couple of challanges and moved them to one file.

--- challange1.js
+++ challange1.js
@@ -18,3 +18,51 @@ function foo() {
 foo(); // What does this return? And why?
 //Answer here:
 //https://nullprogram.com/blog/2012/11/19/ 
+
+//There is a bug in the below code preventing the factors of 18 from being displayed accurately. Can you spot it? 
+var dividend = 18;
+var numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+var idx;
+
+for (idx = 0; idx < numbers.length; idx++) {
+  var factor;
+  var divisor = numbers[idx];
+
+  if (dividend % divisor === 0) {
+    factor = true;
+  }
+
+  if (factor) {
+    console.log(divisor + ' is a factor of ' + dividend + '!');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
